62d2741c563fa7c6ec346d3554eb426782167018
dldmstlr20419@naver.com <dldms@DESKTOP-O794MR4>
2025-03-09 00:52:19 +0900

    000

--- JAVA/src/Object/Member2.java
+++ JAVA/src/Object/Member2.java
@@ -1,17 +1,10 @@
 package Object;
 import lombok.Data;
-import lombok.AllArgsConstructor;
-
 import lombok.NonNull;
 @Data
 
-@AllArgsConstructor
 public class Member2 {
-	
 	private String id;
 	@NonNull private String name;
 	private int age;
-	public String getId() {
-		return id;
-	}
 }
