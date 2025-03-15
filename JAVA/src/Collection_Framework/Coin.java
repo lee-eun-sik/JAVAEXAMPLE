ab81b0886d34066073523008144a4c5b106040a4
dldmstlr20419@naver.com <dldms@DESKTOP-O794MR4>
2025-03-15 21:09:30 +0900

    1

---
+++ JAVA/src/Collection_Framework/Coin.java
@@ -0,0 +1,13 @@
+package Collection_Framework;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
