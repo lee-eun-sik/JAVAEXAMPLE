fe8f7634dc84648f289c18124e962f8093880f01
Admin <Admin@DESKTOP-EEKJFHH>
2025-04-25 10:06:31 +0900

    00

---
+++ sp5_chap03/src/main/java/main/AlreadyExistingMemberException.java
@@ -0,0 +1,10 @@
+package main;
+
+public class AlreadyExistingMemberException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6543043264806252421L;
+
+}
---
+++ sp5_chap03/src/main/java/main/IdPasswordNotMatchingException.java
@@ -0,0 +1,10 @@
+package main;
+
+public class IdPasswordNotMatchingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 98775197720078397L;
+
+}
